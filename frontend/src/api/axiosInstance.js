782410da60ffadc5f0f79d6dab31a609147edc02
Vishesh Jain <Visheshjain0405@gmail.com>
2025-07-06 17:18:23 +0530

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 // You can adjust this to point to your backend’s base URL.
 // If you have a REACT_APP_API_URL in your .env, it will be used; otherwise default to localhost.
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+//const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const baseURL = process.env.REACT_APP_API_URL || 'https://panisho-project.onrender.com/api';
+
 
 const api = axios.create({
   baseURL,
